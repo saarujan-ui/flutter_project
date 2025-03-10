8c14838d004c90738716c922f142020756b054bd
InukaHansaja <Inukahansaja@gmail.com>
2025-03-10 21:37:53 +0530

    Update main.py

--- Backend/signup_backend/main.py
+++ Backend/signup_backend/main.py
@@ -7,4 +7,4 @@ app.include_router(user_router, prefix="/user", tags=["User"])
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Signup API"}
+    return {"message": "Welcome to the Registration"}
