d8b2409537227520c83728bde0c9418944aab081
InukaHansaja <Inukahansaja@gmail.com>
2025-03-11 09:37:23 +0530

    Update database.py

--- Backend/Backend/app/database.py
+++ Backend/Backend/app/database.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
 from app.config import Config
 
-
+client = MongoClient(Config.MONGO_URI)
+db = client.mysdgp  # Select database
+users_collection = db.users  # Select users collection
